aaea119fc2a9c59caaca2987f0caefa31c67e477
vitekambroz <vitek.ambroz1@email.cz>
2025-11-12 11:07:58 +0100

    sss

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,10 +1,12 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
 
+  // Přesměrování z borel.cz → www.borel.cz (včetně podstránek a query parametrů)
   if (url.hostname === "borel.cz") {
     url.hostname = "www.borel.cz";
     return Response.redirect(url.toString(), 301);
   }
 
+  // Pokračuj dál, pokud už je to www
   return context.next();
 }
